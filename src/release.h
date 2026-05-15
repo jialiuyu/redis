a106cacf58d014a9eb6e622ee5fd2f9e9330612b
szza <790909829@qq.com>
2026-05-15 17:34:52 +0800

    Stop tracking generated local build artifacts

--- src/release.h
+++
@@ -1,5 +0,0 @@
-#define REDIS_GIT_SHA1 "992d4018"
-#define REDIS_GIT_DIRTY "     863"
-#define REDIS_BUILD_ID "fibonaccii.local-1777456497"
-#include "version.h"
-#define REDIS_BUILD_ID_RAW REDIS_VERSION REDIS_BUILD_ID REDIS_GIT_DIRTY REDIS_GIT_SHA1
